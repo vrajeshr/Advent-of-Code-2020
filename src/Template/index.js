ddcee90cbd5ba73198363efab0359d5f2a39808f
Vrajesh Rana <vur2@njit.edu>
2020-12-02 12:54:07 -0500

    added template file

---
+++ src/Template/index.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+input = [];
+try {
+    const data = fs.readFileSync(`${__dirname}/input.txt`, "UTF-8");
+
+    const lines = data.split(/\r?\n/);
+
+    lines.forEach((line) => {
+        input.push(line);
+    });
+} catch (err) {
+    console.error(err);
+}
+
+const solution = () => {
+    return console.log("Hello world!");
+};
+
+solution();
